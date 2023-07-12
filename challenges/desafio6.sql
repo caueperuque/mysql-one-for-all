70906c1574534e85800ba086fc8713a634ab0a0a
caue peruque <caue19052003@gmail.com>
2023-07-11 22:03:32 -0300

    requisito 6 finalizado

--- challenges/desafio6.sql
+++ challenges/desafio6.sql
@@ -1,5 +1,5 @@
 SELECT
-	MIN(p.plan_value) AS faturamento_minimo,
+	  MIN(p.plan_value) AS faturamento_minimo,
     MAX(p.plan_value) AS faturamento_maximo,
     ROUND(AVG(p.plan_value), 2) AS faturamento_medio,
     ROUND(SUM(p.plan_value), 2) AS faturamento_total
